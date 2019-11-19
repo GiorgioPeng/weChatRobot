54fed1bef2ae2e0b989de2d6b40da8dfc5e65d53
Giorgio <GiorgioPeng@github.com>
2019-11-19 23:22:06 +0800

    modified:   wxRb.py

--- wxRb.py
+++ wxRb.py
@@ -1,5 +1,5 @@
 from wxpy import *
 if __name__ == '__main__':
-	b = Bot(cache_path=True)
-	for i in range(len(b.user_details(b.friends()))):
-		print(b.user_details(b.friends())[i])
+    b = Bot(cache_path=True)
+    for i in b.friends():
+        print(i)
